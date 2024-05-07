9869ccc3424b453115ec200562141fb654face98
sparqxi <zhongxi1992@gmail.com>
2024-05-07 04:12:11 -0700

    test deploy

---
+++ settings.tsx
@@ -0,0 +1,14 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "popcat", mintText: "$popcat holder mint", buttonLabel: "Mint", header: "Pop cat holder mint",price:0.1 },
+  { label: "OGs", mintText: "Whitelist mint", buttonLabel: "Mint", header: "Whitelist Mint",price:0.1 },
+  { label: "public", mintText: "Mint Paying with DEF", buttonLabel: "Mint", header: "Public Mint",price:0.15 },
+  { label: "default", mintText: "default mintText", buttonLabel: "Mint", header: "Default",price:0.15}
+];
+
+//header image in the ui. replace with your own
+export const image = "/unnamed.png";
+
+//website title
+export const headerText = "PopCat mint ";
